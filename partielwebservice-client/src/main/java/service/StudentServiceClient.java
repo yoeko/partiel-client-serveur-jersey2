fc18da9166ec1a00935e43c8d54105993244d0e7
yoeko <jklu@zenetys.com>
2020-12-10 00:31:13 +0100

    Add create student method

--- partielwebservice-client/src/main/java/service/StudentServiceClient.java
+++ partielwebservice-client/src/main/java/service/StudentServiceClient.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.ClientConfig;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 import domaine.Etudiant;
@@ -19,6 +22,7 @@ public class StudentServiceClient {
 
 	
 	private String url = "http://localhost:8080/partielwebservice-webservice/rest/json/student/";
+
 	
 	public List<Etudiant> getListStudent(){
 		
@@ -33,6 +37,29 @@ public class StudentServiceClient {
 		  List<Etudiant> listEtudiant = response.readEntity(new GenericType<List<Etudiant>>() {});
 		  
 		  return listEtudiant;
+		
+	}
+	
+	
+	public void createStudent(Etudiant student)
+	{
+
+		Client client = ClientBuilder.newClient();
+
+		WebTarget webTarget = client.target(url).path("create");
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String input;
+		
+		try {
+			input = objectMapper.writeValueAsString(student);
+			System.out.println(input);
+			Response response = webTarget.request("application/json").post(Entity.json(input));
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		
 	}
 
